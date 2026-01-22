303057eec6a32f3f0589c5e84d3a195189fbcc7e
Sathwik061 <madiresathwik@gmail.com>
2026-01-22 19:25:59 +0530

    Innitial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+---
+title: Extractive Text Summarization App
+emoji: 📝
+colorFrom: blue
+colorTo: green
+sdk: gradio
+sdk_version: "4.44.0"
+python_version: "3.11"
+app_file: app.py
+pinned: false
+---
+
+# Extractive Text Summarization App
+
+This project implements an extractive text summarization system using NLP techniques such as tokenization, stopword removal, and frequency-based sentence scoring.
+
+## Features
+- Extractive summarization
+- Built with Python and Gradio
+- Deployed on Hugging Face Spaces
+
+## Usage
+Paste a document into the textbox and get a concise extractive summary.
---
+++ app.py
@@ -0,0 +1,72 @@
+import gradio as gr
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from collections import defaultdict
+
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
+
+stop_words = set(stopwords.words("english"))
+lemmatizer = WordNetLemmatizer()
+
+def preprocess_sentence(sentence):
+    sentence = sentence.lower()
+    words = word_tokenize(sentence)
+    words = [
+        lemmatizer.lemmatize(word)
+        for word in words
+        if word.isalpha() and word not in stop_words
+    ]
+    return words
+
+def frequency_based_summarizer(text, num_sentences=5):
+    sentences = sent_tokenize(text)
+
+    word_freq = defaultdict(int)
+    for sentence in sentences:
+        for word in preprocess_sentence(sentence):
+            word_freq[word] += 1
+
+    sentence_scores = {}
+    for i, sentence in enumerate(sentences):
+        for word in preprocess_sentence(sentence):
+            sentence_scores[i] = sentence_scores.get(i, 0) + word_freq[word]
+
+    top_indices = sorted(
+        sentence_scores,
+        key=sentence_scores.get,
+        reverse=True
+    )[:num_sentences]
+
+    top_indices.sort()
+    summary = " ".join([sentences[i] for i in top_indices])
+    return summary
+
+def read_text_file(file):
+    if file is None:
+        return ""
+    
+    with open(file.name, 'r', encoding='utf-8', errors='ignore') as f:
+        return f.read()
+# Main Summarization Function (APP Logic)
+def summarize_uploaded_document(file):
+    text = read_text_file(file)
+
+    if len(text.strip()) == 0:
+        return "please upload a valid text file."
+
+    summary = frequency_based_summarizer(text, num_sentences=5)
+
+    return summary      
+interface = gr.Interface(
+    fn=summarize_uploaded_document,
+    inputs=gr.File(label="Upload a Text Document (.txt)"),
+    outputs=gr.Textbox(label="Generated Summary"),
+    title="Extractive Text Summarization App",
+    description="Upload a document and automatically generate a concise summary using NLP-based extractive summarization."
+)
+
+interface.launch()
